8f50f2edcaf3ce50a913ed717d4b83341f5524db
Alexey <12871810+alexeyhimself@users.noreply.github.com>
2024-07-06 01:32:50 +0300

    prettified code before new development

--- src/js/manage_links/manage_links.js
+++ src/js/manage_links/manage_links.js
@@ -5,25 +5,29 @@ function draw_links_error_message() {
   return `<p id="links_placeholder"><span style="font-size: 2em;">😲</span><br><b>Something went wrong...</b><br>Please <a href="#" id="copy_error_message_to_clipboard">click here to copy an error message to clipboard</a> and <a href="mailto:alexeyhimself@gmail.com">let us know</a></p>`;
 }
 
+function enable_copy_error_message_to_clipboard_listener(element_id, error_message) {
+  var element = document.getElementById(element_id);
+  element.addEventListener('click', function (event) {
+    navigator.clipboard.writeText(error_message);
+  });
+}
+
 function draw_links() {
-  let links_html = '';
   try {
     const links = load_links_from_local_storage_sorted_by();
 
     if (links.length == 0)
-      links_html = draw_links_placeholder();
+      document.getElementById("links_area").innerHTML = draw_links_placeholder();
     else
-      links_html = draw_existing_links(links);
+      document.getElementById("links_area").innerHTML = draw_existing_links(links);
 
     if (links.length > 10) {
       document.getElementById("number_of_links").innerHTML = links.length;
       document.getElementById("links_export").style.display = '';
     }
-    document.getElementById("links_area").innerHTML = links_html;
   } catch (error) {
     console.error(error);
-    links_html = draw_links_error_message();
-    document.getElementById("links_area").innerHTML = links_html;
+    document.getElementById("links_area").innerHTML = draw_links_error_message();
     enable_copy_error_message_to_clipboard_listener("copy_error_message_to_clipboard", error);
   }
 }
@@ -95,12 +99,6 @@ function calculate_links_stats(importance) {
 }
 */
 
-function enable_copy_error_message_to_clipboard_listener(element_id, error_message) {
-  var element = document.getElementById(element_id);
-  element.addEventListener('click', function (event) {
-    navigator.clipboard.writeText(error_message);
-  });
-}
 
 function enable_manage_links() {
   enable_buttons_listeners({
