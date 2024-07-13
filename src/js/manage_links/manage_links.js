815f1a427e65f10509ca56710d53fbaf0479532d
Alexey <12871810+alexeyhimself@users.noreply.github.com>
2024-07-13 12:46:48 +0300

    changed watch icon

--- src/js/manage_links/manage_links.js
+++ src/js/manage_links/manage_links.js
@@ -75,7 +75,7 @@ function draw_links(links, no_links_callback) {
 function draw_existing_grouped_links(grouped_links) {
   let links_html = '';
   let i = 1;
-  const what_to_do_map = {"read": "📚", "watch": "📺", "listen": "🎧"};
+  const what_to_do_map = {"read": "📚", "watch": "🖥️", "listen": "🎧"};
   //const what_to_do_map = {"read": '<i class="bi bi-book-half"></i>', "watch": '<i class="bi bi-youtube"></i>', "listen": '<i class="bi bi-earbuds"></i>'};
 
   ["read", "watch", "listen", "others"].forEach((what_to_do) => {
