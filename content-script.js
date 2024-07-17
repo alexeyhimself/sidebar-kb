9b2fe65312ffce2a1bdc8636411fb0f506907659
Alexey <12871810+alexeyhimself@users.noreply.github.com>
2024-07-17 22:48:20 +0300

    demo

--- content-script.js
+++ content-script.js
@@ -8,3 +8,8 @@ button.addEventListener('click', function () {
 });
 document.body.append(button);
 */
+
+const style = "padding-top: 22px; font-weight: bold; border: 1px solid black; z-index: 100500; position: fixed; width: 110px; height: 110px; background: yellow; border-radius: 50%; top: 10px; right: 10px; color: black; text-align: center; font-size: 14px;";
+const html = `<div style="${style}">Exists in<br> a Knowledge Base</div>`;
+const element = new DOMParser().parseFromString(html, 'text/html').body.firstElementChild;
+document.body.append(element);
