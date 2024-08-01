f56be42c7a6ba7887aa9eb4b2710d1a1a0adb448
Alexey <12871810+alexeyhimself@users.noreply.github.com>
2024-08-01 23:33:40 +0300

    more fixes

--- src/js/collect_links/collect_links.js
+++ src/js/collect_links/collect_links.js
@@ -194,4 +194,8 @@ function enable_collect_links() {
   //draw_links_stats_chart_under_priority_bar("chart_what_to_do");
   fill_stats_of_what_to_do_for_links();
   //count_tabs_in_a_window();
+
+  //var myAlert = document.getElementById('toast');//select id of toast
+  //var bsAlert = new bootstrap.Toast(myAlert);//inizialize it
+  //bsAlert.show();//show it
 }
