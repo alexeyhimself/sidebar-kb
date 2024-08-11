a571bac893451e502b236a0b2164c36ba0f4d930
Alexey <12871810+alexeyhimself@users.noreply.github.com>
2024-08-11 13:34:24 +0300

    partial bug fix for previous commit

--- src/js/queue_to_learn/queue_to_learn.js
+++ src/js/queue_to_learn/queue_to_learn.js
@@ -163,9 +163,9 @@ function draw_link_in_queue_tab(item, j, what_to_do) {
   }
 
   const hostname = get_hostname(item.link);
-  links_html += ` <span class="hostname">${hostname}</span> <a href="${item.link}" target="_blank">${item.title.trim()}</a><!--&nbsp;&nbsp;| <a href="#" data-url="${item.link}" class="edit_in_queue"--><!--i class="bi bi-pencil"></i--><!--img src="/images/pencil.svg" style="height: 16px; padding-right: 0px; padding-bottom: 2px;">&nbsp;Edit...</a-->`;
+  links_html += ` <span class="hostname">${hostname}</span> <a class="queue-link-a" href="${item.link}" target="_blank">${item.title.trim()}</a><!--&nbsp;&nbsp;| <a href="#" data-url="${item.link}" class="edit_in_queue"--><!--i class="bi bi-pencil"></i--><!--img src="/images/pencil.svg" style="height: 16px; padding-right: 0px; padding-bottom: 2px;">&nbsp;Edit...</a-->`;
 
-  links_html += `<div class="btn-group" style="float: right; position: relative; top: 10px; width: auto;"> \
+  links_html += `<div class="btn-group queue-link-menus" style="float: inline-end; position: relative; top: 10px; width: auto;"> \
   <button type="button" class="btn btn-light dropdown-toggle dropdown-toggle-split-" data-bs-toggle="dropdown" aria-expanded="false" style="padding-: 6px;"> \
     <!--span class="visually-hidden">Toggle Dropdown</span--> \
   </button> \
