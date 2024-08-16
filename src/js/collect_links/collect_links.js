78fda85178c5dc87dffbb09dd8a4605549c0d8b4
Alexey <12871810+alexeyhimself@users.noreply.github.com>
2024-08-16 03:03:33 +0300

    fixed edit buttons for existing link

--- src/js/collect_links/collect_links.js
+++ src/js/collect_links/collect_links.js
@@ -206,13 +206,10 @@ function adjust_if_link_already_exists(link) {
   const existing_link = link_already_exists(link);
   if (existing_link) {
     fill_the_collect_links_form_with_existing_data(existing_link);
-    document.getElementById("save").innerText = `Update in ${sources_map[existing_link.source]}`;
+    //document.getElementById("save").innerText = `Update in ${sources_map[existing_link.source]}`;
     //document.getElementById("save").classList.add("btn-success");
     document.getElementById("save").dataset.source = existing_link.source;
-  }
-  else {
-    document.getElementById("save").innerText = "Save";
-    //document.getElementById("save").classList.remove("btn-success");
+    show_move_and_delete_buttons();
   }
 }
 
