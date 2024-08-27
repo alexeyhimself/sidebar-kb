adc2f1eca55c5e62b9407b3d027e4a5427574405
Alexey <12871810+alexeyhimself@users.noreply.github.com>
2024-08-28 00:55:25 +0300

    Fixes for AI after new API docs published

--- src/js/commons/ai.js
+++ src/js/commons/ai.js
@@ -1,3 +1,8 @@
+/************************************************ DOCS *************************************************
+ * Gemini Nano AI: https://docs.google.com/document/d/1VG8HIyz361zGduWgNG7R_R8Xkv0OOJ8b5C9QKeCjU0c/edit
+ * Gemini AI: https://ai.google.dev
+ *******************************************************************************************************/
+
 const GEMINI_API_KEY = undefined;
 const GEMINI_API_URL = "https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent";
 const GEMINI_API_TIMEOUT = 5000; // do not wait for a response longer than 5 seconds
@@ -5,12 +10,14 @@ var available_ai_platforms = {};
 
 
 async function check_availability_of_gemini_nano_ai() {
-  if (!window.ai)
+  if (!ai)
     return;
 
   try {
-    const session = await window.ai.assistant.create();
+    // (await ai.assistant.capabilities()).available == "readily";
+    const session = await ai.assistant.create();
     available_ai_platforms["gemini_nano"] = true;
+    session.destroy();
   } 
   catch (error) {
     console.warn(`Gemini Nano AI is not available due to an error: ${error}`);
@@ -52,19 +59,21 @@ async function check_available_ai_platforms() {
 
 async function ask_ai_gemini_nano(payload) {
   try {
-    const session = await window.ai.assistant.create();
-    const question = `On a resource: "${payload.link}" we have a webpage titled: "${payload.title}". \
-                      And we want to compose meaningful tags for this page. \
+    const session = await ai.assistant.create();
+    const question = `On URL: "${payload.link}" we have a webpage titled: "${payload.title}". \
+                      And we want to compose meaningful tags for this webpage. \
                       Advise several tags (at least 3, at most 10) that mostly (but not necessarily) \
                       made of the words used in these title and resource. \
                       Each tag could be made of 1 or 2 words only. \
                       Each tag could be only a noun. Do not use adjectives for tags. \
-                      Do not offer resource itself as a tag. \
+                      Do not offer neither URL value nor "URL" as a tag. \
                       Do not use hypen to concatenate several words as 1 tag. \
                       Do not concatenate several words without spaces as 1 tag. \
-                      Do not offer "URL" or "Title" as tags unless these words exist in the title. \
+                      Do not offer "URL" or "Title" as tags. \
                       Return only array of comma separated tags as a response.`;
     let answer = await session.prompt(question);
+    session.destroy();
+
     console.log(answer);
     answer = answer.replace(/\[|\]|```json|```|- /g, '');  // if we ask for a "comma separated list only" then anything could be in return (; separated, \n- separated, etc). So, by now we ask for an array, but remove []
     answer = answer.replace(/\n/g, ',');
